8d5cfa74e22410c9ab2f2ccfef0eee785d757d48
ArmanProject <114120551+ArmanProject@users.noreply.github.com>
2022-09-22 09:33:56 +0700

    Add files via upload

---
+++ eyes.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+# code by ArmanProject
+# script tampilan eyes
+
+# tampilan
+tam1 ="                                   .::!!!!!!!:.
+  .!!!!!:.                        .:!!!!!!!!!!!!
+  ~~~~!!!!!!.                 .:!!!!!!!!!UWWW$$$
+      :$$NWX!!:           .:!!!!!!XUWW$$$$$$$$$P
+      $$$$$##WX!:      .<!!!!UW$$$$"  $$$$$$$$#
+      $$$$$  $$$UX   :!!UW$$$$$$$$$   4$$$$$*
+      ^$$$B  $$$$\     $$$$$$$$$$$$   d$$R"
+        "*$bd$$$$      '*$$$$$$$$$$$o+#"
+             """"          """"""""
+tam2 ="======================"
+
+# login
+toilet -f big -F gay LOGIN
+echo "masukan password" | lolcat
+read pass
+
+# data tampilan
+if [ pass = Arman098 ]
+them
+    echo "masukan title"
+    read title
+    clear
+    figlet $title | lolcat
+    echo $tam1 | lolcat = tampilan 1
+    echo $tam2 | lolcat = tampilan 2
+    echo $tam1 | lolcat = tampilan 1
+    echo
+    echo "# security cyber" | lolcat
+    echo "# Creator by = ArmanProject" | lolcat
+    echo "# saya masih bocil bang" | lolcat
+    echo $tam1 | lolcat # tampilan 1
+else
+    echo "password salah" | lolcat
+    echo $tam1 | lolcat # tampilan 1
+    sh eyes.sh
+fi
